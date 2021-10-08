149fa8c6506eabfb1689c00c9d2736eaf4a4ae60
李鑫玮 <>
2021-10-08 16:56:43 +0800

    环境搭建

---
+++ foodie-api/src/main/java/com/imooc/Application.java
@@ -0,0 +1,19 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author lixinwei
+ * @Date 2021/10/8 4:32 下午
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ foodie-api/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author lixinwei
+ * @Date 2021/10/8 4:34 下午
+ * @Version 1.0
+ **/
+@RestController//默认所有请求返回出去的都是一个json对象，是@ResponseBody和@Controller的组合注解
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Object hello() {
+        return "Hello World";
+    }
+}
