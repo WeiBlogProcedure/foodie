10379e6864e8a8a9d694d03fc26fe6e528d30ff1
李鑫玮 <>
2021-10-12 00:09:00 +0800

    逆向工具生成mapper、mapper.xml、pojo

---
+++ foodie-mapper/src/main/java/com/imooc/mapper/ItemsSpecMapper.java
@@ -0,0 +1,9 @@
+package com.imooc.mapper;
+
+import com.imooc.my.mapper.MyMapper;
+import com.imooc.pojo.ItemsSpec;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ItemsSpecMapper extends MyMapper<ItemsSpec> {
+}
